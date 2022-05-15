dd672a1ca49e002e05b7412be58b1b320e2a19fb
Igor Babichev <babichevI66@gmail.com>
2022-05-15 15:45:00 -0400

    Revert "Wrong code"
    This reverts commit 2a02465c79930a65d401a615baf97d9fdf981dc1.

--- app/src/main/java/com/ib/roomdatabasewithnav/MainActivity.kt
+++ app/src/main/java/com/ib/roomdatabasewithnav/MainActivity.kt
@@ -15,7 +15,5 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragment)
         return navController.navigateUp() || super.onSupportNavigateUp()
-
-        this is a wrong code
     }
 }
